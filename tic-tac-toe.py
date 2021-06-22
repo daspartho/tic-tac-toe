5a97c141dea56f340a3a231f27e417687290b190
Partho Das <parthodas6176@gmail.com>
2021-06-22 11:09:17 +0530

    Added helful comments

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,26 +1,32 @@
 def create_board():
+    # creates an array representing the board
     global board
     board = [x for x in range(9)]
     
 def print_board():
-    for i in range(0,7,3):
+    # prints board
+    for i in range(0,7,3): 
         for j in board[i:i+3]:
             print(j,end= '  ')
-        print('\n')
+        print('\n') # change line after every 3rd character
 
 def insert_letter(letter, pos):
+    # Inserts letter at position
     board[pos] = letter
 
 def is_free(pos):
-    return board[pos]==pos
+    # Checks if the position if free
+    return board[pos]==pos # returns True or False value
 
 def is_full():
+    # Checks if the board is full
     for i in board:
         if type(i) == int:
             return False
     return True
 
 def move(letter):
+    # Asks user to input a move and validate it, if valid adds it to board
     while True:
         pos = int(input(f'Enter a position to place an {letter} (0-8): '))
         assert pos in range(9)
@@ -30,13 +36,16 @@ def move(letter):
         else:
             print('Position already occupied!')
             
-def winner():
+def winner(): # returns winning letter if there is a winner else returns None
+    # Checks rows for a straight line
     for i in range(3):
         if board[i]==board[i+3]==board[i+6]:
             return board[i]
+    # Checks columns for a straight line
     for i in range(0,7,3):
         if board[i]==board[i+1]==board[i+2]:
             return board[i]
+    # Checks diagonals for a straight line
     if board[0]==board[4]==board[8]:
         return board[0]
     if board[2]==board[4]==board[6]:
@@ -44,21 +53,24 @@ def winner():
     return None
 
 def main():
+    # Main game loop
     print('Welcome to Tic Tac Toe!')
     print('The board has positions 0-8 starting from the top left.')
     print()
     create_board()
     print_board()
-    while not is_full() and not winner():
+    while not is_full() and not winner(): # run until someone wins or the board is full
         move('X')
         print()
         print_board()
-        if not is_full() and not winner():
-            move('O')
-            print()
-            print_board()
+        if is_full() or winner():
+            # breaks out of the loop if 'X' alredy won by the last move or the board got full
+            break
+        move('O')
+        print()
+        print_board()
     if winner():
         print(f'{winner()} wins!')
     elif is_full():
-        print('Draw')
+        print('Tie! No more spaces left.')
 main()
