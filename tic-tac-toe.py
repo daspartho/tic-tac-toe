fa4925220417ca21461e0874301f0157efcd32a8
mbcalli <mbcalli@clemson.edu>
2021-06-28 15:47:53 -0400

    removed use of global variable

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,31 +1,30 @@
 def create_board():
     # creates an array representing the board
-    global board
-    board = [x for x in range(9)]
+    return [x for x in range(9)]
     
-def print_board():
+def print_board(board):
     # prints board
     for i in range(0,7,3): 
         for j in board[i:i+3]:
             print(j,end= '  ')
         print('\n') # change line after every 3rd character
 
-def insert_letter(letter, pos):
+def insert_letter(letter, pos, board):
     # Inserts letter at position
     board[pos] = letter
 
-def is_free(pos):
+def is_free(pos, board):
     # Checks if the position if free
     return board[pos]==pos # returns True or False value
 
-def is_full():
+def is_full(board):
     # Checks if the board is full
     for i in board:
         if type(i) == int:
             return False
     return True
 
-def move(letter):
+def move(letter, board):
     # Asks user to input a move and validate it, if valid adds it to board
     while True:
         pos = int(input(f'Enter a position to place an {letter} (0-8): '))
@@ -34,13 +33,13 @@ def move(letter):
         except AssertionError:
             print('Invalid position! Try Again!')
             continue # try again
-        if is_free(pos):
-            insert_letter(letter, pos)
+        if is_free(pos, board):
+            insert_letter(letter, pos, board)
             break
         else:
             print('Position already occupied!')
             
-def winner(): # returns winning letter if there is a winner else returns None
+def winner(board): # returns winning letter if there is a winner else returns None
     # Checks rows for a straight line
     for i in range(3):
         if board[i]==board[i+3]==board[i+6]:
@@ -61,21 +60,21 @@ def main():
     print('Welcome to Tic Tac Toe!')
     print('The board has positions 0-8 starting from the top left.')
     print()
-    create_board()
-    print_board()
-    while not is_full() and not winner(): # run until someone wins or the board is full
-        move('X')
+    board = create_board()
+    print_board(board)
+    while not is_full(board) and not winner(board): # run until someone wins or the board is full
+        move('X', board)
         print()
-        print_board()
-        if is_full() or winner():
+        print_board(board)
+        if is_full(board) or winner(board):
             # breaks out of the loop if 'X' alredy won by the last move or the board got full
             break
-        move('O')
+        move('O', board)
         print()
-        print_board()
-    if winner():
-        print(f'{winner()} wins!')
-    elif is_full():
+        print_board(board)
+    if winner(board):
+        print(f'{winner(board)} wins!')
+    elif is_full(board):
         print('Tie! No more spaces left.')
         
 while True:
